fc830590fbc53862d951260a68b3af1b9da65133
soundharyasureshkumar <soundharya1718@gmail.com>
2020-01-10 17:39:13 +0530

    second commit

--- app/src/main/java/com/example/chatboxapplication/MainActivity.java
+++ app/src/main/java/com/example/chatboxapplication/MainActivity.java
@@ -4,10 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    RoomListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        private String channelID = "n19iPjGKuTbhcDN7";
+        private String roomName = "observable-room";
+        private EditText editText;
+        private Scaledrone scaledrone;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            // This is where we write the mesage
+            editText = (EditText) findViewById(R.id.editText);
+        }
+
+        // Successfully connected to Scaledrone room
+        @Override
+        public void onOpen(Room room) {
+            System.out.println("Conneted to room");
+        }
+
+        // Connecting to Scaledrone room failed
+        @Override
+        public void onOpenFailure(Room room, Exception ex) {
+            System.err.println(ex);
+        }
+
+        // Received a message from Scaledrone room
+        @Override
+        public void onMessage(Room room, com.scaledrone.lib.Message receivedMessage) {
+            // TODO
+        }
 }
