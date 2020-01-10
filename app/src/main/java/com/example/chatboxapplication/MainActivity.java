99e67071c7bb4c5eea905e8fdd3533895c45b939
soundharyasureshkumar <soundharya1718@gmail.com>
2020-01-10 17:52:47 +0530

    third commit

--- app/src/main/java/com/example/chatboxapplication/MainActivity.java
+++ app/src/main/java/com/example/chatboxapplication/MainActivity.java
@@ -36,4 +36,30 @@ public class MainActivity extends AppCompatActivity {
         public void onMessage(Room room, com.scaledrone.lib.Message receivedMessage) {
             // TODO
         }
+        private String getRandomName() {
+            String[] adjs = {"autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling",  "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"};
+            String[] nouns = {"waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"};
+            return (
+                    adjs[(int) Math.floor(Math.random() * adjs.length)] +
+                            "_" +
+                            nouns[(int) Math.floor(Math.random() * nouns.length)]
+            );
+        }
+
+        private String getRandomColor() {
+            Random r = new Random();
+            StringBuffer sb = new StringBuffer("#");
+            while(sb.length() < 7){
+                sb.append(Integer.toHexString(r.nextInt()));
+            }
+            return sb.toString().substring(0, 7);
+        }
+    }
+    public void sendMessage(View view) {
+        String message = editText.getText().toString();
+        if (message.length() > 0) {
+            scaledrone.publish("observable-room", message);
+            editText.getText().clear();
+        }
+    }
 }
---
+++ app/src/main/java/com/example/chatboxapplication/MessageAdapter.java
@@ -0,0 +1,87 @@
+public class MessageAdapter extends BaseAdapter {
+
+    List<Message> messages = new ArrayList<Message>();
+    Context context;
+
+    public MessageAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void add(Message message) {
+        this.messages.add(message);
+        notifyDataSetChanged(); // to render the list we need to notify
+    }
+
+    @Override
+    public int getCount() {
+        return messages.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return messages.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    // This is the backbone of the class, it handles the creation of single ListView row (chat bubble)
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        MessageViewHolder holder = new MessageViewHolder();
+        LayoutInflater messageInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        Message message = messages.get(i);
+
+        if (message.isBelongsToCurrentUser()) { // this message was sent by us so let's create a basic chat bubble on the right
+            convertView = messageInflater.inflate(R.layout.my_message, null);
+            holder.messageBody = (TextView) convertView.findViewById(R.id.message_body);
+            convertView.setTag(holder);
+            holder.messageBody.setText(message.getText());
+        } else { // this message was sent by someone else so let's create an advanced chat bubble on the left
+            convertView = messageInflater.inflate(R.layout.their_message, null);
+            holder.avatar = (View) convertView.findViewById(R.id.avatar);
+            holder.name = (TextView) convertView.findViewById(R.id.name);
+            holder.messageBody = (TextView) convertView.findViewById(R.id.message_body);
+            convertView.setTag(holder);
+
+            holder.name.setText(message.getMemberData().getName());
+            holder.messageBody.setText(message.getText());
+            GradientDrawable drawable = (GradientDrawable) holder.avatar.getBackground();
+            drawable.setColor(Color.parseColor(message.getMemberData().getColor()));
+        }
+
+        return convertView;
+    }
+    public void onMessage(Room room, final JsonNode json, final Member member) {
+        // To transform the raw JsonNode into a POJO we can use an ObjectMapper
+        final ObjectMapper mapper = new ObjectMapper();
+        try {
+            // member.clientData is a MemberData object, let's parse it as such
+            final MemberData data = mapper.treeToValue(receivedMessage.getMember().getClientData(), MemberData.class);
+            // if the clientID of the message sender is the same as our's it was sent by us
+            boolean belongsToCurrentUser = receivedMessage.getClientID().equals(scaledrone.getClientID());
+            // since the message body is a simple string in our case we can use json.asText() to parse it as such
+            // if it was instead an object we could use a similar pattern to data parsing
+            final Message message = new Message(receivedMessage.getData().asText(), data, belongsToCurrentUser);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    messageAdapter.add(message);
+                    // scroll the ListView to the last added element
+                    messagesView.setSelection(messagesView.getCount() - 1);
+                }
+            });
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+class MessageViewHolder {
+    public View avatar;
+    public TextView name;
+    public TextView messageBody;
+}
